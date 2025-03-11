43ac8f5c84652bb430fd078f0948c8c0193845a0
vadsha <heinzwe2626@gmail.com>
2025-03-11 15:55:59 +0630

    refined quick sort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,11 +1,18 @@
-function quickSort (array) {
-  if(array.length < 2) {
+function quickSort(array) {
+  if (array.length < 2) {
     return array;
   }
   const pivot = array[Math.floor(array.length / 2)];
-  const smallerItems = array.filter((item) => item < pivot);
-  const largerItems = array.filter((item) => item > pivot);
-  return [...quickSort(smallerItems) , pivot , ...quickSort(largerItems)];
+  const smallerItems = [];
+  const largerItems = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) {
+      smallerItems.push(array[i]);
+    } else if (array[i] > pivot) {
+      largerItems.push(array[i]);
+    }
+  }
+  return [...quickSort(smallerItems), pivot, ...quickSort(largerItems)];
 }
 
-console.log(quickSort([1,3,5,7,9,2,4,6,8,10]));
+console.log(quickSort([1, 3, 5, 7, 9, 2, 4, 6, 8, 10]));
